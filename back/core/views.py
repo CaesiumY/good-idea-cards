d40688934ee355556497158032193cc926e493f0
caesiumy <mn0658@naver.com>
2019-12-31 21:17:36 +0900

    feat: Add pick randomly

--- back/core/views.py
+++ back/core/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Post, Draft
 from .serializer import postSerializer, draftSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+import random
 
 # Create your views here.
+
+
+def pickRandom():
+    return random.randrange(1, Post.objects.all().count() + 1)
 
 
 class postsViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = postSerializer
+
+    def get_queryset(self):
+        return Post.objects.all().filter(id=pickRandom())
 
 
 class draftsViewset(viewsets.ModelViewSet):
