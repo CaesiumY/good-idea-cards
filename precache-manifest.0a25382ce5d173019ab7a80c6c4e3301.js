b45a07e40df0fd783c6c2d5c721f26e3ba907fd9
caesiumy <mn0658@naver.com>
2020-01-01 00:05:32 +0900

    Updates

---
+++ precache-manifest.0a25382ce5d173019ab7a80c6c4e3301.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67783eadc5fa5ec7487f232909e97d73",
+    "url": "/good-idea-cards/index.html"
+  },
+  {
+    "revision": "5f47c15a45c58eeaed70",
+    "url": "/good-idea-cards/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "6aee9c8d3bb501d5ad2a",
+    "url": "/good-idea-cards/static/css/main.9511f89c.chunk.css"
+  },
+  {
+    "revision": "5f47c15a45c58eeaed70",
+    "url": "/good-idea-cards/static/js/2.60c83b4f.chunk.js"
+  },
+  {
+    "revision": "3e01856dd46799eedc35ec9da08dd851",
+    "url": "/good-idea-cards/static/js/2.60c83b4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6aee9c8d3bb501d5ad2a",
+    "url": "/good-idea-cards/static/js/main.21779feb.chunk.js"
+  },
+  {
+    "revision": "58bb8fae7806082f0b7e",
+    "url": "/good-idea-cards/static/js/runtime-main.9778135b.js"
+  }
+]);
