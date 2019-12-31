887c140109c696bdb72016222dc4d5cd6c7bb736
caesiumy <mn0658@naver.com>
2019-12-31 23:32:35 +0900

    feat: Ready for drafts

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Draft from "./routes/Draft";
 import SideNav from "./components/SideNav";
+import { Layout } from "antd";
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <HashRouter>
-          <SideNav></SideNav>
-          <Route path="/" exact={true} component={Home}></Route>
-        </HashRouter>
+        <Layout style={{ minHeight: "100vh" }}>
+          <HashRouter>
+            <SideNav></SideNav>
+            <Route path="/" exact={true} component={Home}></Route>
+            <Route path="/dratfs" component={Draft}></Route>
+          </HashRouter>
+        </Layout>
       </>
     );
   }
---
+++ front/src/routes/Draft.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Empty } from "antd";
+
+export default class Draft extends Component {
+  render() {
+    return (
+      <div>
+        <Empty />
+        <p>hi</p>
+      </div>
+    );
+  }
+}
