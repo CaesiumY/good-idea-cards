4bdde45ec0f2f310d33cce6913a4ad80b0792d23
caesiumy <mn0658@naver.com>
2019-12-31 21:48:55 +0900

    feat: Add spinner

--- front/src/App.js
+++ front/src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import api from "./api";
 import Posts from "./components/Posts";
 import AppLayout from "./container/AppLayout";
+import { Spin, Alert } from "antd";
 
 export default class App extends Component {
   state = {
+    isLoading: true,
     results: []
   };
 
   getData = async () => {
     const data = await api.getAllPosts();
-    console.log(data.data);
-    this.setState({ results: data.data });
+    this.setState({ results: data.data, isLoading: false });
   };
 
   componentDidMount() {
@@ -19,21 +20,30 @@ export default class App extends Component {
   }
 
   render() {
-    const { results } = this.state;
+    const { results, isLoading } = this.state;
     return (
       <>
         <AppLayout>
-          <h1>hello world!</h1>
-          <div className="posts">
-            {results.map(item => (
-              <Posts
-                id={item.id}
-                author={item.author}
-                content={item.content}
-                key={item.id}
+          {isLoading ? (
+            <Spin tip="Loading...">
+              <Alert
+                message="데이터를 불러오는 중"
+                description="시간이 오래걸린다면 F5를 눌러 새로고침해주세요."
+                type="info"
               />
-            ))}
-          </div>
+            </Spin>
+          ) : (
+            <div className="posts">
+              {results.map(item => (
+                <Posts
+                  id={item.id}
+                  author={item.author}
+                  content={item.content}
+                  key={item.id}
+                />
+              ))}
+            </div>
+          )}
         </AppLayout>
       </>
     );
