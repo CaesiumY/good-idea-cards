ebe3ab303b60fee218bc6ae4cccdd54e2192a4a9
caesiumy <mn0658@naver.com>
2019-12-31 23:32:20 +0900

    feat: Add Post Card

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
+import { Card } from "antd";
 
 export default class Posts extends Component {
-  componentDidMount() {
-    console.log(this.props);
-  }
-
   render() {
     const { id, author, content } = this.props;
     return (
       <div className="post">
-        <p className="post__id">{id}</p>
-        <h3 className="post__author">{author}</h3>
-        <p className="post__content">{content}</p>
+        <Card title={id} style={{ width: 300 }}>
+          <h2 className="post__author">{author}</h2>
+          <p className="post__content">{content}</p>
+        </Card>
       </div>
     );
   }
