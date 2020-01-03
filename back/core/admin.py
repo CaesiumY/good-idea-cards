64c9d7e65d9aaf96b8b0b6f07452fbde4381ec4d
caesiumy <mn0658@naver.com>
2020-01-04 00:24:28 +0900

    feat: Add import export format

--- back/core/admin.py
+++ back/core/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Post, Draft
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Draft)
+# admin.site.register(Post)
+# admin.site.register(Draft)
+
+
+@admin.register(Draft)
+@admin.register(Post)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
