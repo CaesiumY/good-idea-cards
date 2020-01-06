08100b991b79c41790a3ae938c74211f3c8f339b
caesiumy <mn0658@naver.com>
2020-01-06 22:04:51 +0900

    feat: Add search input logic

--- front/src/routes/Search.js
+++ front/src/routes/Search.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
-import { Empty, Input } from "antd";
+import { Empty, Input, Spin } from "antd";
+import Posts from "../components/Posts";
 
 const { Search } = Input;
 
 export default class SearchContainer extends Component {
+  state = {
+    isLoading: false,
+    results: []
+  };
+
   render() {
+    const { isLoading, results } = this.state;
     return (
-      <div>
-        <Empty />
-        <p style={{ textAlign: "center" }}>서비스 준비중입니다...</p>
-      </div>
+      <>
+        <Search
+          placeholder="검색어를 입력해주세요."
+          onSearch={value => console.log(value)}
+          enterButton
+          size="large"
+        />
+        {isLoading ? (
+          <Spin tip="Loading..."></Spin>
+        ) : (
+          <div className="posts">
+            {results.map(item => (
+              <Posts
+                id={item.id}
+                author={item.author}
+                content={item.content}
+                key={item.id}
+              />
+            ))}
+          </div>
+        )}
+        {results ? <Empty style={{ marginTop: "75px" }} /> : "success"}
+      </>
     );
   }
 }
