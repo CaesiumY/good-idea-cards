21cd361526dac212714023d051a8d3c4e33124a3
caesiumy <mn0658@naver.com>
2020-01-07 22:32:31 +0900

    feat: Update PostCard title id

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -11,13 +11,13 @@ export default class Posts extends Component {
   render() {
     const { id, author, content } = this.props;
     return (
-      <div className="post">
-        <PostCard title={id} bordered={false}>
-          <h2 className="post__author">
+      <div>
+        <PostCard title={`${id}번 카드`} bordered={false}>
+          <h2>
             <Icon type="caret-right" />
             {author}
           </h2>
-          <p className="post__content">{content}</p>
+          <p>{content}</p>
         </PostCard>
       </div>
     );
