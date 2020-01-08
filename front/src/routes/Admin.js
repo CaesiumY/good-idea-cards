e540336a2c0d5b15f3e65dc5d3a998e8f103ca43
caesiumy <mn0658@naver.com>
2020-01-08 21:53:01 +0900

    feat: Add popconfirms

--- front/src/routes/Admin.js
+++ front/src/routes/Admin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Table, Divider, Button } from "antd";
+import { Table, Divider, Button, Popconfirm, message } from "antd";
 import api from "../api";
 
 const { Column } = Table;
@@ -27,6 +27,10 @@ export default class Admin extends Component {
     this.setState({ selectedRowKeys });
   };
 
+  confirmDelete = e => {
+    message.error("삭제되었습니다.");
+  };
+
   componentDidMount() {
     this.getData();
   }
@@ -46,15 +50,23 @@ export default class Admin extends Component {
             추가
           </Button>
           <Divider type="vertical" />
-          <Button type="danger" onClick={this.start} disabled={!hasSelected}>
-            삭제
-          </Button>
-
-          <span style={{ marginLeft: 8 }}>
+          <Popconfirm
+            title={`${selectedRowKeys.length}개의 아이템을 정말로 삭제하시겠습니까?`}
+            onConfirm={this.confirmDelete}
+            okText="예"
+            cancelText="아니오"
+            placement="topLeft"
+          >
+            <Button type="danger" onClick={this.start} disabled={!hasSelected}>
+              삭제
+            </Button>
+          </Popconfirm>
+          <span style={{ marginLeft: 15 }}>
             {hasSelected ? `${selectedRowKeys.length} 개의 아이템 선택됨` : ""}
           </span>
         </div>
         <Table dataSource={results} rowSelection={rowSelection}>
+          <Column title="Id" dataIndex="id" key="id" />
           <Column title="Author" dataIndex="author" key="author" />
           <Column title="Content" dataIndex="content" key="content" />
 
@@ -62,11 +74,19 @@ export default class Admin extends Component {
             rowSelection={rowSelection}
             title="Action"
             key="action"
-            render={(text, record) => (
+            render={(text, record, index) => (
               <span>
                 <Button type="default">추가</Button>
                 <Divider type="vertical" />
-                <Button type="danger">삭제</Button>
+                <Popconfirm
+                  title={`${record.id}번 항목을 정말로 삭제하시겠습니까?`}
+                  onConfirm={this.confirmDelete}
+                  okText="예"
+                  cancelText="아니오"
+                  placement="topRight"
+                >
+                  <Button type="danger">삭제</Button>
+                </Popconfirm>
               </span>
             )}
           />
