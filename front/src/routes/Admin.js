8c31f574f3f19bf2101ec006b102b2cab3184eb5
caesiumy <mn0658@naver.com>
2020-01-08 21:16:25 +0900

    feat: Add getDraft data

--- front/src/routes/Admin.js
+++ front/src/routes/Admin.js
@@ -1,7 +1,51 @@
 import React, { Component } from "react";
+import { Table, Divider, Button } from "antd";
+import api from "../api";
+
+const { Column } = Table;
 
 export default class Admin extends Component {
+  state = {
+    isLoading: true,
+    results: []
+  };
+
+  getData = async () => {
+    await api
+      .getAllDrafts()
+      .then(response => {
+        this.setState({ results: response.data });
+      })
+      .catch(e => {
+        alert(e);
+      });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
   render() {
-    return <div>this is Admin</div>;
+    const { results } = this.state;
+    return (
+      <>
+        <Table dataSource={results}>
+          <Column title="Author" dataIndex="author" key="author" />
+          <Column title="Content" dataIndex="content" key="content" />
+
+          <Column
+            title="Action"
+            key="action"
+            render={(text, record) => (
+              <span>
+                <Button type="default">Invite {record.lastName}</Button>
+                <Divider type="vertical" />
+                <Button type="danger">Delete</Button>
+              </span>
+            )}
+          />
+        </Table>{" "}
+      </>
+    );
   }
 }
