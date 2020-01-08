2932e919863c57c78ddd7268fa3a508b7ad9d7a1
caesiumy <mn0658@naver.com>
2020-01-08 21:30:39 +0900

    feat: Add Admin table set

--- front/src/routes/Admin.js
+++ front/src/routes/Admin.js
@@ -7,7 +7,8 @@ const { Column } = Table;
 export default class Admin extends Component {
   state = {
     isLoading: true,
-    results: []
+    results: [],
+    selectedRowKeys: []
   };
 
   getData = async () => {
@@ -21,26 +22,51 @@ export default class Admin extends Component {
       });
   };
 
+  onSelectChange = selectedRowKeys => {
+    console.log("selectedRowKeys changed: ", selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  };
+
   componentDidMount() {
     this.getData();
   }
 
   render() {
-    const { results } = this.state;
+    const { results, selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+
     return (
       <>
-        <Table dataSource={results}>
+        <div style={{ marginBottom: 16 }}>
+          <Button type="primary" onClick={this.start} disabled={!hasSelected}>
+            추가
+          </Button>
+          <Divider type="vertical" />
+          <Button type="danger" onClick={this.start} disabled={!hasSelected}>
+            삭제
+          </Button>
+
+          <span style={{ marginLeft: 8 }}>
+            {hasSelected ? `${selectedRowKeys.length} 개의 아이템 선택됨` : ""}
+          </span>
+        </div>
+        <Table dataSource={results} rowSelection={rowSelection}>
           <Column title="Author" dataIndex="author" key="author" />
           <Column title="Content" dataIndex="content" key="content" />
 
           <Column
+            rowSelection={rowSelection}
             title="Action"
             key="action"
             render={(text, record) => (
               <span>
-                <Button type="default">Invite {record.lastName}</Button>
+                <Button type="default">추가</Button>
                 <Divider type="vertical" />
-                <Button type="danger">Delete</Button>
+                <Button type="danger">삭제</Button>
               </span>
             )}
           />
