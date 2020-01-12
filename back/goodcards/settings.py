981d9dcfa40ce31713960b0ee3b1fa7c52501796
caesiumy <mn0658@naver.com>
2020-01-12 21:40:06 +0900

    deploy: Complete deploy to heroku

--- back/goodcards/settings.py
+++ back/goodcards/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if(DEBUG == True):
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 # CORS settings
 if(DEBUG == True):
